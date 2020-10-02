649a253c4a60248ecdf663bf0b887ac05ed775d3
taiwocoker <taiwofcoker@gmail.com>
2020-10-02 11:58:37 +0100

    Add content for App Component

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
 function App() {
+  
   return (
-    <h1 className="App">
-      Calculator
-    </h1>
+    <div className="App">
+      <Display />
+      <ButtonPanel />
+    </div>
   );
+
 }
 
 export default App;
