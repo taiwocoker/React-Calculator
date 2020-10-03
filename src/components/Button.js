a27773005e59e13f947b9f76fdc9ffa0b614b3a3
taiwocoker <taiwofcoker@gmail.com>
2020-10-03 10:18:15 +0100

    Dynamically rendered each button width

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function Button({ buttonName }) {
   return (
-    <button type="button">
+    <button type="button" className={`button text-center text-black ${buttonName === '0' && 'button1'}`}>
       {buttonName}
     </button>
   );
