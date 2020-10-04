bc13495b0d3d63890107d76bd7a57073ac4a286a
taiwocoker <taiwofcoker@gmail.com>
2020-10-04 21:55:19 +0100

    Refactored operate to use basic operations

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,16 +1,20 @@
 import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
-  const x = new Big(numberOne);
-  const y = new Big(numberTwo);
+//   const x = new Big(numberOne);
+//   const y = new Big(numberTwo);
 
-  if (operation === '-') return x.minus(y);
+//   if (operation === '-') return x.minus(y);
 
-  if (operation === '+') return x.plus(y);
+//   if (operation === '+') return x.plus(y);
 
-  if (operation === 'X') return x.mul(y);
+//   if (operation === 'x') return x.mul(y);
 
-  return x.div(y);
+//   return x.div(y);
+  if (operation === '-') return numberOne - numberTwo;
+  if (operation === '+') return numberOne + numberTwo;
+  if (operation === 'x') return numberOne * numberTwo;
+  return numberOne / numberTwo
 };
 
 export default operate;
