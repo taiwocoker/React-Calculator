edd357bb7dfc0b4dde923364d68fffa602f2d0b9
taiwocoker <taiwofcoker@gmail.com>
2020-10-04 17:20:16 +0100

    Add calculate module and destructure the argument

---
+++ src/logic/calculate.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function calculate({data, buttonName}) {
+    const {total, next, operation} = data
+    // const result = total buttonName next
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+
