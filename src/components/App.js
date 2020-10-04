639de898bb37ba9ea6050bad02f54d9c5d8eaa35
taiwocoker <taiwofcoker@gmail.com>
2020-10-04 18:31:49 +0100

    implement the calculate and operate module

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../index.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate'
 
 function App() {
   return (
